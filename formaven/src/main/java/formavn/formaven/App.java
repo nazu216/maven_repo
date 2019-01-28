2543074bef7eafdee5b42102ffeef4417e1f79a9
Your Name <maheshperasani@gmail.com>
2019-01-28 19:38:34 +0530

    commit1

---
+++ formaven/src/main/java/formavn/formaven/App.java
@@ -0,0 +1,13 @@
+package formavn.formaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ formaven/src/main/java/formavn/formaven/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author SCUBE
+ *
+ */
+package formavn.formaven;
